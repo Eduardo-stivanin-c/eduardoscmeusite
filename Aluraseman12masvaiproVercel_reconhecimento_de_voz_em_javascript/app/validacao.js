ffb567fd885705db46503e5ad99255df9da988e5
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-18 11:23:27 -0300

    validacao

--- Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/validacao.js
+++ Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/validacao.js
@@ -1,5 +1,26 @@
 //validacao.js
 
 function    verificaSeChutePossueValorValido(chute) {
+ const numero=+chute
+ if (chuteForInvalido(numero)) {
+    console.log("Valor inválido")
+ }  
+ 
+ 
+ if (numeroForMaiorOuMenorQueOValorPermitido(numero)) 
+ {
+ 
     
+
+console.log(`valor inválido:o número secreto precisa estar entre ${menor-valor} e ${maior-valor}`)
+
+ }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero>maiorValor||numero <menorValor
 }
