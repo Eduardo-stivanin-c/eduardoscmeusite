986e00db187066cdeb1aaa14ad43d6355d053ed2
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-18 16:39:57 -0300

    gambiara

--- Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/gambiara.js
+++ Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/gambiara.js
@@ -1,9 +1,10 @@
-var gambiara=document.getElementsByClassName("box")
+const gambiara=document.getElementsByClassName("box");
 function planoB(){
     if (numero==String(numeroSecreto+'.') ||gambiara[0].innerHTML==String(numeroSecreto)) {
         document.body.innerHTML=`<h2>Você acertou!</h2> <h3>O número secreto era ${numeroSecreto}</h3>` 
     
     }
 
-    return `<h2>Você acertou!</h2> <h3>O número secreto era ${numeroSecreto}</h3>`;
+    return "<h2>Você acertou!</h2> <h3>O número secreto era"  + numeroSecreto + "</h3>";
+
 }
