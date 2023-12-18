afb2940494f64a1292aa717ecf67882be373f130
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-18 16:36:16 -0300

    gambiara

--- Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/gambiara.js
+++ Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/gambiara.js
@@ -1,8 +1,9 @@
 var gambiara=document.getElementsByClassName("box")
 function planoB(){
-    if (gambiara[0].innerHTML==String(numeroSecreto+'.') ||gambiara[0].innerHTML==String(numeroSecreto)) {
+    if (numero==String(numeroSecreto+'.') ||gambiara[0].innerHTML==String(numeroSecreto)) {
         document.body.innerHTML=`<h2>Você acertou!</h2> <h3>O número secreto era ${numeroSecreto}</h3>` 
     
     }
 
+    return `<h2>Você acertou!</h2> <h3>O número secreto era ${numeroSecreto}</h3>
 }
