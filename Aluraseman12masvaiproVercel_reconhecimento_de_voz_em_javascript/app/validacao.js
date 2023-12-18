b9e0a66a6595ad492ac618e716b4aad4a99d97e0
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-18 14:37:01 -0300

    validação

--- Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/validacao.js
+++ Aluraseman12masvaiproVercel_reconhecimento_de_voz_em_javascript/app/validacao.js
@@ -24,9 +24,9 @@ console.log(`Valor inválido:o número secreto precisa estar entre ${menorValor}
 
  // o ou é adapatação minha pro Edge
     //condiçaáo Google chrome condição edge 
-let condicao=(numero==numeroSecreto ||numero==numeroSecreto+'.' )
-console.log(condicao)
-    if(numero==numeroSecreto ||numero==numeroSecreto+'.' ){
+//let condicao=(numero==numeroSecreto ||numero==numeroSecreto+'.' )
+//console.log(condicao)
+    if(numero==numeroSecreto){
 
 //se condição é valida
 
